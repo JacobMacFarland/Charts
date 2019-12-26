75f6d82cfa87f92b81bddc8bfd1dacde520d308b
Jacob MacFarland <macfarlandjw@appstate.edu>
2019-12-26 17:57:12 -0500

    First commit

---
+++ Charts/PieChartView.swift
@@ -0,0 +1,20 @@
+//
+//  PieChartView.swift
+//  Charts
+//
+//  Created by Jacob MacFarland on 12/25/19.
+//  Copyright © 2019 Jacob MacFarland. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PieChartView: UIView {
+    public override init(frame: CGRect) {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Charts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Charts
+//
+//  Created by Jacob MacFarland on 12/25/19.
+//  Copyright © 2019 Jacob MacFarland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
