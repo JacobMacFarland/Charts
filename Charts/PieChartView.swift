dab3f424d89f093eecd6995d4888c83296afad74
Jacob MacFarland <macfarlandjw@appstate.edu>
2019-12-29 14:51:11 -0800

    Migrated code from ViewController to PieChartView

--- Charts/PieChartView.swift
+++ Charts/PieChartView.swift
@@ -11,21 +11,67 @@ import UIKit
 
 class PieChartView: BaseView {
     
-    var pieChart = UIView()
+    var piePath = UIBezierPath()
+    let pieCenter = CGPoint()
+    let pieRadius = CGFloat()
+    //let totalSum = Double()
+    let runningSum = Double()
     
     /*override init() {
         super.init()
     }*/
     
-    public init(radius: Double, backgroundColor: UIColor, x: Double, y: Double) {
-        super.init()
-        pieChart.frame = CGRect(x: x, y: y, width: radius, height: radius)
-        pieChart.backgroundColor = backgroundColor
-        pieChart.layer.cornerRadius = CGFloat(radius) / 2
-        pieChart.layer.masksToBounds = true
-        
-        addSubview(pieChart)
-    }
 
-   
+    
+    public init(radius: Double/*, x: Double, y: Double, data: [Double], colors: [UIColor]*/) {
+        super.init()
+        let pieColors = [UIColor.red, UIColor.green, UIColor.gray, UIColor.blue, UIColor.black, UIColor.purple, UIColor.yellow]
+        let data = [10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0]
+        var totalSum = data.reduce(0, +)
+        print(totalSum)
+        var startAngle: CGFloat = 0
+        
+        
+        for (index, color) in pieColors.enumerated() {
+            
+            let piePath = UIBezierPath()
+            let center = CGPoint(x: 135, y: 330)
+            var endAngle: CGFloat = 360
+            
+            if (index <= 5) {
+                endAngle = CGFloat((Double(data[index + 1])) / 7.0 * 360.0).radians()
+            }
+            
+            print(startAngle)
+            print(endAngle)
+            print("--------")
+            let radius = CGFloat(150)
+            
+            piePath.move(to: center)
+            piePath.addArc(withCenter: center,
+                           radius: radius,
+                           startAngle: startAngle,
+                           endAngle: endAngle,
+                           clockwise: true)
+            piePath.close()
+            
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.path = piePath.cgPath
+            shapeLayer.strokeColor = UIColor.white.cgColor
+            shapeLayer.fillColor = pieColors[index].cgColor
+            shapeLayer.lineWidth = 4
+            
+            self.layer.addSublayer(shapeLayer)
+            
+            startAngle = CGFloat(Double(data[index]) / totalSum  * 360.0).radians()
+
+        }
+    }
+}
+
+extension CGFloat {
+    func radians() -> CGFloat {
+        let b = CGFloat(Double.pi) * (self/180)
+        return b
+    }
 }
--- Charts/ViewController.swift
+++ Charts/ViewController.swift
@@ -13,39 +13,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //let pie = PieChartView(radius: 200, backgroundColor: UIColor.red, x: 110, y: 300)
+        /*let pieColors = [UIColor.red, UIColor.green, UIColor.gray, UIColor.blue, UIColor.black, UIColor.purple, UIColor.yellow]
+        let data: [Double] = [10, 20, 30, 40, 50, 60, 70]
+        let centerX = Double(self.view.frame.width / 2)
+        let centerY = Double(self.view.frame.height / 2)
+        let pie = PieChartView(radius: 100, x: centerX, y: centerY, data: data, colors: pieColors)
+        self.view.addSubview(pie)*/
+        /*let pieColors = [UIColor.red, UIColor.green, UIColor.gray, UIColor.blue, UIColor.black, UIColor.purple, UIColor.yellow]
         
-        let piePath = UIBezierPath()
-        let center = CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2)
-        let startAngle = CGFloat(180).radians()
-        let endAngle = CGFloat(90).radians()
-        let radius = CGFloat(150)
-        
-        piePath.move(to: center)
-        piePath.addArc(withCenter: center,
-                       radius: radius,
-                       startAngle: startAngle,
-                       endAngle: endAngle,
-                       clockwise: true)
-        piePath.close()
-        
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = piePath.cgPath
-        shapeLayer.strokeColor = UIColor.black.cgColor
-        shapeLayer.fillColor = UIColor.orange.cgColor
-        shapeLayer.lineWidth = 2
-        
-        self.view.layer.addSublayer(shapeLayer)
-
+        for (index, color) in pieColors.enumerated() {
+            let piePath = UIBezierPath()
+            let center = CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2)
+            let startAngle = CGFloat(Double(index) / 7.0 * 360.0).radians()
+            let endAngle = CGFloat((Double(index) + 1.0) / 7.0 * 360.0).radians()
+            let radius = CGFloat(150)
+            
+            piePath.move(to: center)
+            piePath.addArc(withCenter: center,
+                           radius: radius,
+                           startAngle: startAngle,
+                           endAngle: endAngle,
+                           clockwise: true)
+            piePath.close()
+            
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.path = piePath.cgPath
+            shapeLayer.strokeColor = UIColor.white.cgColor
+            shapeLayer.fillColor = UIColor.orange.cgColor
+            shapeLayer.lineWidth = 4
+            
+            self.view.layer.addSublayer(shapeLayer)
+        }*/
+        let pieChart = PieChartView(radius: 2)
+        self.view.addSubview(pieChart)
     }
 
 
 }
-
-extension CGFloat {
-    func radians() -> CGFloat {
-        let b = CGFloat(Double.pi) * (self/180)
-        return b
-    }
-}
-
