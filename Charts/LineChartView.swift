f6c8ee6e15393c65e1c9f86c561ca85e1624fda8
Jacob MacFarland <macfarlandjw@appstate.edu>
2019-12-30 15:44:19 -0800

    Added rudimentary test way to add ticks to line chart view

--- Charts/LineChartView.swift
+++ Charts/LineChartView.swift
@@ -13,6 +13,9 @@ class LineChartView: BaseView {
     public init (x: Double, y: Double, height: Double, width: Double, data: [Double]) {
         super.init()
         
+        let numYTicks = 5
+        let numXTicks = 10
+        
         let mainYPath = UIBezierPath(rect: CGRect(x: x, y: y, width: 1, height: height))
         let mainYLayer = CAShapeLayer()
         mainYLayer.path = mainYPath.cgPath
@@ -25,6 +28,29 @@ class LineChartView: BaseView {
         mainXLayer.strokeColor = UIColor.black.cgColor
         mainXLayer.fillColor = UIColor.black.cgColor
         
+        
+        for i in 0...numYTicks {
+            let currYLevel = y + Double(i) / Double(numYTicks) * height
+            let yTickPath = UIBezierPath(rect: CGRect(x: x - 5, y: currYLevel, width: 5, height: 1))
+            
+            let yTickLayer = CAShapeLayer()
+            yTickLayer.path = yTickPath.cgPath
+            yTickLayer.strokeColor = UIColor.black.cgColor
+            yTickLayer.fillColor = UIColor.black.cgColor
+            self.layer.addSublayer(yTickLayer)
+        }
+        
+        for i in 0...numXTicks {
+            let currXLevel = x + Double(i) / Double(numXTicks) * width
+            let xTickPath = UIBezierPath(rect: CGRect(x: currXLevel, y: y + height, width: 1, height: 5))
+            
+            let xTickLayer = CAShapeLayer()
+            xTickLayer.path = xTickPath.cgPath
+            xTickLayer.strokeColor = UIColor.black.cgColor
+            xTickLayer.fillColor = UIColor.black.cgColor
+            self.layer.addSublayer(xTickLayer)
+        }
+    
         self.layer.addSublayer(mainYLayer)
         self.layer.addSublayer(mainXLayer)
         
