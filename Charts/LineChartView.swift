1c27b48ff89606aa013bb8a56a7d4d3ea3945765
Jacob MacFarland <macfarlandjw@appstate.edu>
2019-12-30 14:29:03 -0800

    Added skeleton class for LineChartView

---
+++ Charts/LineChartView.swift
@@ -0,0 +1,14 @@
+//
+//  LineChartView.swift
+//  Charts
+//
+//  Created by Jacob MacFarland on 12/30/19.
+//  Copyright © 2019 Jacob MacFarland. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class LineChartView: BaseView {
+
+}
